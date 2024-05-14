1c4b6c9048d3eff72128a30300621540e524fbbe
Sameer Faridi <s.faridi007@gmail.com>
2024-05-14 23:08:54 +0530

    websites i create  page

--- components/edited-videos.tsx
+++ components/edited-videos.tsx
@@ -1,5 +1,8 @@
 import { Separator } from "./ui/separator"
 import YouTube from 'react-youtube';
+import Image from "next/image";
+import project01 from '../public/project-1.png'
+import project02 from '../public/project-2.png'
 
 export const EditedVideos = () => {
     const videoOptions = {
@@ -18,17 +21,17 @@ export const EditedVideos = () => {
       };
     return (
         <div className="text-center">
-                <h1 className="pb-4 font-bold tracking-tight text-5xl lg:text-6xl ">Videos I've edited</h1>
+                <h1 className="pb-4 font-bold tracking-tight text-5xl lg:text-6xl ">Websites I Create</h1>
                 <div className="flex items-center justify-center">
                     <Separator className="mt-3 bg-slate-100/20 h-0.5 w-40" />
                 </div>
 
                 <div className="flex justify-center">
                     <div className="mt-10 grid sm:grid-cols-1 lg:grid-cols-2 gap-5">
-                        <YouTube videoId="Siexk0ZFKCw" opts={videoOptions}/>
-                        <YouTube videoId="C0cbOOecml8" opts={videoOptions}/>
-                        <YouTube videoId="SjqmjAq2YAE" opts={videoOptions}/>
-                        <YouTube videoId="ofbDgjXAs5s" opts={videoOptions}/>
+                       <Image src={project01} alt="project one"/>
+                       <Image src={project02} alt="project one"/>
+                       <Image src={project02} alt="project one"/>
+                       <Image src={project01} alt="project one"/>
                     </div>
                 </div>
             </div>
