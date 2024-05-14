f7b0cb959c88e861cdbf1eeab815ee09970f2f79
Sameer Faridi <s.faridi007@gmail.com>
2024-05-14 23:27:40 +0530

    add companys

--- components/channels.tsx
+++ components/channels.tsx
@@ -5,27 +5,27 @@ import { Avatar } from "@mui/material"
 export const Channels = () => {
     const channels = [
         {
-            logo: '/alpha.jpeg',
-            name: 'Alpha',
-            subs: '200,000',
-            link: 'https://www.youtube.com/@AlphaCentralOfficial'
+            logo: '/kurmato_logo.png',
+            name: 'Kurmato',
+            subs: 'Dec 23 - now',
+            link: 'https://kurmato.com/'
         },
         {
-            logo: '/frazer.jpeg',
-            name: 'Brookes',
-            subs: '70,000',
-            link: 'https://www.youtube.com/@FrazerBrookesChannel'
+            logo: '/4achievers_logo.webp',
+            name: '4Archivers',
+            subs: 'July 23 - Dec 23',
+            link: 'https://4archivers.com/'
         },
         {
-            logo: '/dan.jpg',
-            name: 'Dan Kieft',
-            subs: '14,000',
+            logo: '/abhiwan_logo.webp',
+            name: 'Abhiwan Technology',
+            subs: 'May 23 - July 23',
             link: 'https://www.youtube.com/@Dankieft'
         },
         {
-            logo: '/joe.jpeg',
-            name: 'Joe Leech',
-            subs: '30,000',
+            logo: '/dahua_logo.png',
+            name: 'Dahua Technology IN',
+            subs: 'Oct 22 - Apr 23',
             link: 'https://www.youtube.com/@Joe_Leech'
         },
     ]
@@ -45,12 +45,12 @@ export const Channels = () => {
       };
     return (
         <div className="text-center">
-                <h1 className="pb-4 font-bold tracking-tight text-5xl lg:text-6xl ">Channels I've Worked With</h1>
+                <h1 className="pb-4 font-bold tracking-tight text-5xl lg:text-6xl ">Companys I've Worked With</h1>
                 <div className="flex items-center justify-center">
                     <Separator className="mt-3 bg-slate-100/20 h-0.5 w-40" />
                 </div>
                
-               <div className="flex items-center justify-center mx-auto sm:space-x-2 md:space-x-4 lg:space-x-8">
+               <div className="flex flex-col sm:flex-row items-center justify-center mx-auto sm:space-x-2 md:space-x-4 lg:space-x-8">
                 {channels.map((channel, index) => (
                     <div key={index} className="mt-10 flex">
                         <div className="flex flex-col items-center">
@@ -58,7 +58,7 @@ export const Channels = () => {
                             <Avatar src={channel.logo} alt={channel.name} sx={style}/>
                             </Link>
                             <h1 className="font-semibold text-xl mt-4">{channel.name}</h1>
-                            <p className="text-lg mt-2">{channel.subs} subs</p>
+                            <p className="text-lg mt-2">{channel.subs}</p>
                         </div>
                     </div>
                 ))}
--- components/edited-shorts.tsx
+++ components/edited-shorts.tsx
@@ -18,7 +18,7 @@ export const EditedShorts = () => {
       };
     return (
         <div className="text-center">
-            <h1 className="pb-4 font-bold tracking-tight text-5xl lg:text-6xl ">Shorts I've edited</h1>
+            <h1 className="pb-4 font-bold tracking-tight text-5xl lg:text-6xl ">Applications I've create</h1>
             <div className="flex items-center justify-center">
                 <Separator className="mt-3 bg-slate-100/20 h-0.5 w-40" />
             </div>
