7f01d16b06aa1f6c686cb1e2b0794afe14cbc5a3
Sameer Faridi <s.faridi007@gmail.com>
2024-05-14 23:00:41 +0530

    changes in home page

--- components/hero.tsx
+++ components/hero.tsx
@@ -11,7 +11,7 @@ export const Hero = () => {
                         <div className="relative inline-flex before:absolute before:inset-0">
                             <Link
                             className="px-3 py-1 text-sm font-medium inline-flex items-center justify-center border rounded-full  text-zinc-300 hover:text-white transition duration-150 ease-in-out w-full group border-slate-100/40"
-                            href="https://twitter.com/BenVFXs"
+                            href="https://www.linkedin.com/in/sameer-faridi"
                             target="_blank"
                             >
                                 <span className="relative inline-flex items-center">
@@ -24,9 +24,9 @@ export const Hero = () => {
                         </div>
                     </div>
                     <h1 className="pb-4 font-extrabold tracking-tight text-transparent text-7xl lg:text-8xl  bg-clip-text bg-gradient-to-r from-cyan-400 via-blue-600 to-cyan-400" data-aos="fade-down">
-                        Edits Made Easy
+                        SAMEER FARIDI
                     </h1>
-                    <p className="mb-8 text-lg text-zinc-300/40 font-medium" data-aos="fade-down" data-aos-delay="200">Transform your videos with retention based editing</p>
+                    <p className="mb-8 text-lg text-zinc-300/40 font-medium" data-aos="fade-down" data-aos-delay="200">A full-stack web and app developer</p>
                     <div className="flex flex-col items-center max-w-xs mx-auto gap-4 sm:max-w-none  sm:justify-center sm:flex-row sm:inline-flex" data-aos="fade-down"
                     data-aos-delay="400">
                         <Link
