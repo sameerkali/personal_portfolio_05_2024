068778506783c0dfdd156ded01423e707e77c9eb
Sameer Faridi <s.faridi007@gmail.com>
2024-05-15 22:50:35 +0530

    add og:Image

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,31 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import Head from "next/head"; // Import Head component
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata: Metadata = {
-  title: "Sameer Faridi",
-  description: "created by sameer faridi",
-};
-
 export default function RootLayout({
-  children,
+  children
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
-    </html>
+    <>
+      <Head>
+        <title>{"Sameer Faridi"}</title>
+        <meta name="description" content={"Created by Sameer Faridi"} />
+
+        <meta
+          property="og:image"
+          content={
+            "https://media.licdn.com/dms/image/D4D03AQGAA6vZ8d-qdA/profile-displayphoto-shrink_400_400/0/1706337921133?e=1721260800&v=beta&t=6RJRqc2hV-rEYPFP7IXEMmgSs3dT5yX2_HE4XHrtc4E"
+          }
+        />
+      </Head>
+      <html lang="en">
+        <body className={inter.className}>{children}</body>
+      </html>
+    </>
   );
 }
